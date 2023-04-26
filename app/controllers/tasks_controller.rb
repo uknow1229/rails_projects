651fd97308eb5d7db2c48802b1439e7b2fcd26fe
saya <uknow.love1229@gmail.com>
2023-04-26 13:25:14 +0900

    [Add]create-action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,19 @@
 class TasksController < ApplicationController
-    def index
-        @tasks = Task.all
-    end
+  def index
+    @tasks = Task.all
+  end
+  def new
+    @task = Task.new
+  end
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to root_path
+  end
+  
+
+  private
+  def task_params
+    params.require(:task).permit(:title)
+  end
 end

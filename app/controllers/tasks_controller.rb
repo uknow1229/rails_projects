37ad614c03f38e59662466aff2c1823c44e252eb
saya <uknow.love1229@gmail.com>
2023-04-26 14:33:28 +0900

    [Add]edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,9 +2,11 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
 end
+
   def new
     @task = Task.new
   end
+
   def create
     @task = Task.new(task_params)
     if @task.save
@@ -13,7 +15,20 @@ end
         render 'new', status: :unprocessable_entity
     end
   end
+
+  def edit
+    # p params[:id]
+    @task = Task.find(params[:id])
+  end
   
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to root_path
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
 
   private
   def task_params
